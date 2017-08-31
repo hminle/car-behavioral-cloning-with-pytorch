d67783b5361f5da83a87f037c76bace4c65e57a6
Hoang Le <lemin1991@postech.ac.kr>
2017-08-31 22:16:01 +0900

    fix random shadow

--- utils.py
+++ utils.py
@@ -97,7 +97,7 @@ def random_shadow(image):
     # as x2 == x1 causes zero-division problem, we'll write it in the below form:
     # (ym-y1)*(x2-x1) - (y2-y1)*(xm-x1) > 0
     mask = np.zeros_like(image[:, :, 1])
-    mask[(ym - y1) * (x2 - x1) - (y2 - y1) * (xm - x1) > 0] = 1
+    mask[np.where((ym - y1) * (x2 - x1) - (y2 - y1) * (xm - x1) > 0)] = 1
 
     # choose which side should have shadow and adjust saturation
     cond = mask == np.random.randint(2)
@@ -128,7 +128,7 @@ def augument(data_dir, center, left, right, steering_angle, range_x=100, range_y
     image, steering_angle = choose_image(data_dir, center, left, right, steering_angle)
     image, steering_angle = random_flip(image, steering_angle)
     image, steering_angle = random_translate(image, steering_angle, range_x, range_y)
-    # image = random_shadow(image) --> error
+    image = random_shadow(image)
     image = random_brightness(image)
     return image, steering_angle
 
