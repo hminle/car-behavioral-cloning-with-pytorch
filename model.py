63ebd929d5b2ea3cb2f7ffe522d5e014d7d22e32
Hoang Le <lemin1991@postech.ac.kr>
2017-08-31 14:58:24 +0900

    add model

---
+++ model.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+from torch.autograd import Variable
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+#from torchnet.meter import AverageValueMeter
+import torch.backends.cudnn as cudnn
+
+
+class CarModel(nn.Module):
+    def __init__(self):
+        super(CarModel, self).__init__()
+        self.conv_layers = nn.Sequential(
+            # input is batch_size x 3 x 66 x 200
+            nn.Conv2d(3, 24, 5, stride=2, bias=False),
+            nn.ELU(0.2, inplace=True),
+            
+            nn.Conv2d(24, 36, 5, stride=2, bias=False),
+            #nn.BatchNorm2d(args.D_features * 2),
+            nn.ELU(0.2, inplace=True),
+            
+            nn.Conv2d(36, 48, 5, stride=2, bias=False),
+            #nn.BatchNorm2d(args.D_features * 4),
+            nn.ELU(0.2, inplace=True),
+            
+            nn.Conv2d(48, 64, 3, stride=1, bias=False),
+            #nn.BatchNorm2d(args.D_features * 8),
+            nn.ELU(0.2, inplace=True),
+            
+            nn.Conv2d(64, 64, 3, stride=1, bias=False),
+            nn.ELU(0.2, inplace=True),
+        )
+        self.linear_layers = nn.Sequential(
+            #input from sequential conv layers
+            nn.Linear(in_features=64*1*18, out_features=100, bias=False),
+            nn.ELU(0.2, inplace=True),
+            nn.Linear(in_features=100, out_features=50, bias=False),
+            nn.ELU(0.2, inplace=True),
+            nn.Linear(in_features=50, out_features=10, bias=False),
+            nn.Linear(in_features=10, out_features=1, bias=False))
+        self._initialize_weights()
+        
+    # custom weight initialization
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                init.normal(m.weight, mean=0, std=0.02)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.normal(m.weight, mean=1, std=0.02)
+                init.constant(m.bias, 0)
+
+    def forward(self, input):
+        output = self.conv_layers(input)
+        output = output.view(output.size(0), 64*1*18)
+        output = self.linear_layers(output)
+        return output
